50054469a59a5869f0737c7a6c6dcf35b9d4f940
Shinyuy Marcel <shinyuy9@gmail.com>
2024-12-13 13:24:24 +0100

    Fix

--- d4medicals/settings.py
+++ d4medicals/settings.py
@@ -260,12 +260,14 @@ LOGGING = {
 
 CORS_ALLOWED_ORIGINS = [  
     'http://localhost:3000',
-    'http://127.0.0.1:3000',  
+    'http://127.0.0.1:3000',
+    'https://d4-medicals.vercel.app'  
 ]
 
 CSRF_TRUSTED_ORIGINS = [     
     'http://localhost:3000',
     'http://127.0.0.1:3000',  
+    'https://d4-medicals.vercel.app'
 ]
 
 # CORS_ORIGIN_ALLOW_ALL=True
